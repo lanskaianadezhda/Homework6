ad3b5789d8c3cb9c9fda9f18e9edb4574642f750
lanskaianadezhda <nadin-91@bk.ru>
2024-09-28 18:32:24 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //task 1
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //task 2
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        //task 3
+        System.out.println("Задача 3");
+
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+
+        //task 4
+        System.out.println("Задача 4");
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
